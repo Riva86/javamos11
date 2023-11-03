6483a8ece3641971ca6a5b188f34a8ac5f5bd55a
Riva86 <112680187+Riva86@users.noreply.github.com>
2023-11-03 11:04:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function calculo (){
+
+    const ticket = 200;
+    let cantidad = document.getElementById('inputCity').value;
+    let subtotal = cantidad*ticket
+    let estudiante =(200*cantidad)-(subtotal*(80/100));
+    let trainee = (200*cantidad)-(subtotal*(50/100));
+    let junior = (200*cantidad)-(subtotal*(15/100));
+    
+    if(document.getElementById("inputState").value == document.getElementById("estudiante").value){
+        resultado.innerText =estudiante;
+    }else if(document.getElementById("inputState").value == document.getElementById("trainee").value){
+        resultado.innerText =trainee;
+    }else if(document.getElementById("inputState").value == document.getElementById("junior").value){   
+        resultado.innerText =junior;
+    }
+}
